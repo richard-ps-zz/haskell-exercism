4d09f1eb7b0b8e61f6f63e43476497a52b941eef
richard <richard.silva@neoway.com.br>
2015-08-20 18:43:31 -0300

    Changing tabs to spaces

--- Atbash.hs
+++ Atbash.hs
@@ -7,8 +7,8 @@ import Data.List
 encode :: String -> String
 encode [] = []
 encode (x:xs) = case (elemIndex x common_alphabet) of
-  					Just n  -> atbash_alphabet !! n : encode xs
-  					Nothing -> "Invalid number!"
-	where 
-		common_alphabet = "abcdefghijklmnopqrstuvwxyz"
-		atbash_alphabet = "zyxwvutsrqponmlkjihgfedcba"
+                    Just n  -> atbash_alphabet !! n : encode xs
+                    Nothing -> "Invalid number!"
+    where 
+        common_alphabet = "abcdefghijklmnopqrstuvwxyz"
+        atbash_alphabet = "zyxwvutsrqponmlkjihgfedcba"
--- Beer.hs
+++ Beer.hs
@@ -7,12 +7,12 @@ sing x y = putStrLn (song x y)
 
 song :: Int -> Int -> String
 song x y
-	| x > y = verse x ++ song (x-1) y
-	| x == y = verse 0
+    | x > y = verse x ++ song (x-1) y
+    | x == y = verse 0
 
 verse :: Int -> String
 verse x
-	  | x /= 0 = (show x) ++ pieces !! 0 ++ (show x) ++ pieces !! 1 ++ show (x-1) ++ pieces !! 2
-	  | x == 0 = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-	  where 
-	  	pieces = [" bottles of beer on the wall, ", " bottles of beer.\nTake one down and pass it around, ", " bottles of beer on the wall.\n\n"]
+      | x /= 0 = (show x) ++ pieces !! 0 ++ (show x) ++ pieces !! 1 ++ show (x-1) ++ pieces !! 2
+      | x == 0 = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+      where 
+      	pieces = [" bottles of beer on the wall, ", " bottles of beer.\nTake one down and pass it around, ", " bottles of beer on the wall.\n\n"]
